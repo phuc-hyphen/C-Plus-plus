bf9084d8bd10977eaa7975b9175e601df9ee5abe
LE huu phuc <huu-phuc.le@epita.fr>
2022-09-22 17:46:23 +0200

    is-prime-prog

---
+++ Is_Prime_Prog/sources.cpp
@@ -0,0 +1,14 @@
+#include "sources.h"
+
+int i;
+bool Is_Prime(int i)
+{
+    if (i <= 2)
+        return true;
+    for (int j = 2; j < i; j++)
+    {
+        if (i % j == 0)
+            return false;
+    }
+    return true;
+}
---
+++ Is_Prime_Prog/sources.h
@@ -0,0 +1,4 @@
+#pragma once
+
+extern int i;
+extern bool Is_Prime(int i);
