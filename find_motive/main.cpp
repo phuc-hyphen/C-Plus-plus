3c795e79e4d406cbfa17f5d246d555e3c9299ef0
LE huu phuc <huu-phuc.le@epita.fr>
2022-09-25 19:17:07 +0200

    update find motive

--- find_motive/main.cpp
+++ find_motive/main.cpp
@@ -1,33 +1,64 @@
-#include <fstream>
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <vector>
 
-int main()
-{
-    std::string path, symbol;
+bool contains(std::string str, std::string substr){
+        int i = 0;
+        int j = 0;
+        int save_j = j;
+        while(i < str.size() && j < substr.size()){
+            if (str[i] == substr[j])
+            {
+                save_j = j;
+                j++;
+            } else
+            {
+                    j = save_j;
+            }
+            i++;
+        }
 
-    std::cout << "Could give the the file path you want to analyze ?"
-              << std::endl;
-    std::cin >> path;
-    std::ifstream file(path);
+        if(j == substr.size())
+            return true;
+        return false;
+}
 
-    if (!file.is_open())
-    {
-        std::cout << "The file " << path << " could not be opened";
-        return 1;
+std::vector<std::string> tokenize(std::ifstream &ifs) {
+    std::vector<std::string> v;
+    std::string tmp;
+    while (ifs.good()){
+        ifs >> tmp;
+        v.push_back(tmp);
+    }
+    return v;
+}
+
+int getNbOcc(std::vector<std::string> v, std::string subStr){
+    int count = 0;
+    for (int i = 0; i < v.size(); i++) {
+        bool b =  contains(v[i], subStr);
+        if (b == 1)
+        {
+            count++;
+        }
     }
 
-    std::cout << "Veuillez entrer un motif svp ?" << std::endl;
-    std::cin >> symbol;
+    return count;
+}
 
-    int count = 0;
-
-    for (std::string word; file >> word;)
-        if (word.find("in") != std::string::npos)
-            count++;
-
-    std::cout << "The file " << path << " contains " << count
-              << " words contanninng the motive '" << symbol << "'"
-              << std::endl;
-    return 0;
+int main(int argc, char *argv[]) {
+    std::ifstream file(argv[1]);
+    if (argc != 3) {
+        std::cout << "Usage : You have to enter only 2 arguments.";
+        return 1;
+    }
+    if (file.is_open()) {
+        std::vector<std::string> tokens = tokenize(file);
+        int res = getNbOcc(tokens, argv[2]);
+        std::cout << "The file " << argv[1] << " contains " << res << " words containing the motive " << argv[2];
+        return 0;
+    }
+    std::cout << "The file " << argv[1] << " could not be opened";
+    return 1;
 }
